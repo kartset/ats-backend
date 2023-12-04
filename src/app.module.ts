2cf1029a9e3e36181afa775fba8c837dd4902999
Kartik Setia <kartset10@gmail.com>
2023-12-04 13:59:45 +0530

    Initialised vacancies module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OrganisationsModule } from './organisations/organisations.module';
 import { EmployeesModule } from './employees/employees.module';
+import { VacanciesModule } from './vacancies/vacancies.module';
 
 @Module({
-  imports: [OrganisationsModule, EmployeesModule],
+  imports: [OrganisationsModule, EmployeesModule, VacanciesModule],
   controllers: [AppController],
   providers: [AppService],
 })
