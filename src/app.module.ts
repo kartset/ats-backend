825fabf15e98dc742c137dcaa0d6f3cc452165b7
Kartik Setia <kartset10@gmail.com>
2023-12-04 14:02:38 +0530

    Initialised candidates module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { OrganisationsModule } from './organisations/organisations.module';
 import { EmployeesModule } from './employees/employees.module';
 import { VacanciesModule } from './vacancies/vacancies.module';
+import { CandidatesModule } from './candidates/candidates.module';
 
 @Module({
-  imports: [OrganisationsModule, EmployeesModule, VacanciesModule],
+  imports: [OrganisationsModule, EmployeesModule, VacanciesModule, CandidatesModule],
   controllers: [AppController],
   providers: [AppService],
 })
