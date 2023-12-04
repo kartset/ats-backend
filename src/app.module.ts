4930c0d5e682f2ca90052ac3d738bd9f460b50e5
Kartik Setia <kartset10@gmail.com>
2023-12-04 14:03:31 +0530

    Initialised skills module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { OrganisationsModule } from './organisations/organisations.module';
 import { EmployeesModule } from './employees/employees.module';
 import { VacanciesModule } from './vacancies/vacancies.module';
 import { CandidatesModule } from './candidates/candidates.module';
+import { SkillsModule } from './skills/skills.module';
 
 @Module({
-  imports: [OrganisationsModule, EmployeesModule, VacanciesModule, CandidatesModule],
+  imports: [OrganisationsModule, EmployeesModule, VacanciesModule, CandidatesModule, SkillsModule],
   controllers: [AppController],
   providers: [AppService],
 })
