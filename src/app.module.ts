048b61697a64571c15b02d63dc51bcfbad22bf2a
Kartik Setia <kartset10@gmail.com>
2023-12-04 13:56:24 +0530

    Initialised employees module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OrganisationsModule } from './organisations/organisations.module';
+import { EmployeesModule } from './employees/employees.module';
 
 @Module({
-  imports: [OrganisationsModule],
+  imports: [OrganisationsModule, EmployeesModule],
   controllers: [AppController],
   providers: [AppService],
 })
