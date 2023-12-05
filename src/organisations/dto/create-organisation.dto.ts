f598d0c5f218e4d77cc8f549a59a13c6a3d29202
Kartik Setia <kartset10@gmail.com>
2023-12-05 13:45:50 +0530

    validators in create org dto

--- src/organisations/dto/create-organisation.dto.ts
+++ src/organisations/dto/create-organisation.dto.ts
@@ -1,6 +1,11 @@
+import { IsOptional } from 'class-validator';
+
 export class CreateOrganisationDto {
   name: string;
+  @IsOptional()
   pan: string;
+  @IsOptional()
   gst: string;
+  @IsOptional()
   description: string;
 }
