16ecad87de63928b75bb8434dc59399a6bac807f
Kartik Setia <kartset10@gmail.com>
2023-12-15 14:04:54 +0530

    updated schema

--- src/skills/entities/skill.entity.ts
+++ src/skills/entities/skill.entity.ts
@@ -2,10 +2,28 @@ import { Schema } from 'mongoose';
 
 export const SkillSchema = new Schema({
   skill: { type: String, required: true },
+  mentions: { type: Number, required: true, default: 1 },
   departments: [{ type: String, required: true }],
+  type: {
+    type: String,
+    enum: [
+      'transferable',
+      'life',
+      'digital',
+      'personal',
+      'social',
+      'physical',
+      'language',
+      'interpersonal',
+      'technical',
+    ],
+    required: true,
+  },
 });
 
 export interface ISkill {
   skill: string;
+  mentions: number;
   departments: string[];
+  type: string;
 }
